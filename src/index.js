eeda77c2fc6d5b32ba2bd29fcd50f0c55f96285d
github-Aleksandr999908 <aleksandr999908@mail.ru>
2023-01-22 10:01:30 +0500

    add file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (!matrix) return []
+
+  return matrix.reduce((arr1, arr2, arr3) => {
+      if (arr3 % 2 == 0) arr1.push(...arr2)
+      else arr1.push(...arr2.reverse())
+      return arr1
+  }, [])
+
+};
